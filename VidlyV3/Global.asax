7f146811e4f83a1e3c89ba009dee6588bec64ac7
Daniel <danielwilkiewicz@gmail.com>
2018-03-21 17:21:12 +0100

    Add project files.

---
+++ VidlyV3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyV3.MvcApplication" Language="C#" %>
---
+++ VidlyV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyV3.Startup))]
+namespace VidlyV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
