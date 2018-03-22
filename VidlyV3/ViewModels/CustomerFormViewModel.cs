1464679bb1dcea8b779001780f8aa3b3cc876df3
Daniel <danielwilkiewicz@gmail.com>
2018-03-22 21:48:09 +0100

    .

---
+++ VidlyV3/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyV3.Models;
+
+namespace VidlyV3.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+
+
+    }
+}
---
+++ VidlyV3/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyV3.Models;
+
+namespace VidlyV3.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Movie> Movies { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
